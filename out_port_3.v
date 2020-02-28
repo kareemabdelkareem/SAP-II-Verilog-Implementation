9b91bbea998db5cf7349e09fbe6cd798e6ef1b7f
HeshamRmah <49096517+HeshamRmah@users.noreply.github.com>
2020-02-28 15:29:43 +0200

    Add files via upload

--- out_port_3.v
+++ out_port_3.v
@@ -1,9 +1,35 @@
 /*
+* Module: out_port_3
 *
+* Brief : The contents of the accumulator can be loaded into port 3, which drives a hexadecimal display. 
+* This allows us to see the processed data.
 *
+* Input :
+* WBUS   = Data from WBUS 
 *
-*
-*
-*
-*
+* Output :
+* out    = Data to Hexadecimal Display
 */
+module out_port_3 (
+					output [7:0] out,
+					input  [7:0] WBUS );
+
+	// Write your Code here :
+	
+	
+	
+endmodule
+/*************************************** Test Bench ***************************************/
+module t_out_port_3 ;
+
+	wire [7:0] out;
+	reg [7:0] WBUS ;
+
+	initial begin 
+		
+		// Write your Test Cases here :
+		
+		
+	end
+
+endmodule
--- out_port_4.v
+++ out_port_4.v
@@ -1,9 +1,42 @@
 /*
+* Module: out_port_4
 *
+* Brief : The contents of the accumulator can also be sent to port 4. 
+* Notice that pin 7 of port 4 sends an ACKNOWLEDGE signal to the hexadecimal encoder. 
+* This ACKNOWLEDGE signal and the READY signal are part of a concept called handshaking. 
+* Also notice the SERIAL OUT signal from pin 0 of port 4. 
 *
+* Input :
+* WBUS = Data from WBUS
 *
-*
-*
-*
-*
+* Output :
+* serial_out = Data out in serial form
+* acknowedge = acknowedge Signal
 */
+module out_port_4 (
+					output serial_out,
+					output acknowedge,
+					input [7:0] WBUS );
+
+	// Write your Code here :
+	
+	
+	
+	
+	
+endmodule
+/*************************************** Test Bench ***************************************/
+module t_out_port_4 ;
+	
+	wire serial_out,acknowedge;
+	reg [7:0] WBUS ;
+	
+	initial begin 
+		
+		// Write your Test Cases here :
+		
+		
+		
+	end
+
+endmodule
