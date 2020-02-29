66b425127d011eabdb812cad2366cb39c8cfc996
kareemabdelkareem <61627929+kareemabdelkareem@users.noreply.github.com>
2020-02-29 17:50:43 +0200

    Update alu.v

--- alu.v
+++ alu.v
@@ -15,9 +15,11 @@
 * Output :
 * result = Data to WBUS.
 * flags  = zero & sign Flags
+
+sel [3] -> 1 if the operation dosnt effect on flags
 */
 module alu (
-			output [7:0]  result,
+			output  [7:0]  wbus,
 			output [1:0]  flags,
 			input  [7:0]  accumulator,
 			input  [7:0]  tmp,
@@ -27,13 +29,35 @@ module alu (
 	parameter High_Impedance = 8'bzzzz_zzzz;
 	parameter Zero_State     = 8'b0000_0000;
 			
-   reg [7:0] result ;
+   reg [7:0] alu_res ;                         // register for the result 
 
    initial begin
-		result <= Zero_State;
+		alu_res <= Zero_State;
    end
    
    	// Write your Code here :
+	
+	assign flags[0] = Sel[3]? flags[0] : ((alu_res == 0)? 1 : 0 );//  zero flag 
+	assign flags[1] = Sel[3]? flags[1] : ((alu_res[7]== 1)? 1 : 0) ;  // sign flag 
+	
+	assign wbus = Eu? alu_res: High_Impedance ;     // DATA TO W BUS IF ENABLE 
+
+	
+	always @(*)
+	case(Sel)
+	4'b0000 : alu_res  <= accumulator + tmp ;    // add x  , x is b , c 
+	4'b0001 : alu_res  <= accumulator - tmp ;    // sub x 
+	4'b0010 : alu_res  <= accumulator & tmp ;       // and x , ani
+	4'b0011 : alu_res  <= accumulator | tmp ;        // or x ,  ori 
+	4'b0100 : alu_res  <=  tmp - 1 ;                 // dcr x , 
+	4'b0101 : alu_res  <=  tmp + 1 ;                 // inc x , 
+	4'b0110 : alu_res  <= accumulator ^ tmp ;        // xor x ,  xori 
+	4'b1110 : alu_res  <=  {accumulator[6:0],accumulator[7]} ;                 // ral 
+	4'b1111 : alu_res  <=  {accumulator[0],accumulator[7:1]};                 // rar
+	
+	
+	
+	endcase
 	
 	
 	
@@ -45,18 +69,30 @@ endmodule
 /*************************************** Test Bench ***************************************/
 module t_alu ;
 
-	wire [7:0]  result;
+	wire [7:0]  wbus;
 	wire [1:0]  flags;
 	reg  [7:0]  tmp;
 	reg  [7:0]  accumulator;
 	reg  [3:0]  Sel;
 	reg         Eu;
 
-	alu ALU (result,flags,accumulator,tmp,Sel,Eu);
+	alu ALU (wbus,flags,accumulator,tmp,Sel,Eu);
 
 	initial begin 
 		
 		// Write your Test Cases here :
+		Eu = 0 ; Sel = 4'b0000 ; accumulator = 5 ; tmp = 5 ;
+		#100  Eu = 1 ; Sel = 4'b0000 ; accumulator = 5 ; tmp = 5 ;     // result = 5 + 5 = 10
+		#100  Eu = 0 ; Sel = 4'b0001 ; accumulator = 7 ; tmp = 5 ;      // result = high due to  eu = 0
+		#100  Eu = 1 ; Sel = 4'b0001 ; accumulator = 10 ; tmp = 5 ;      // result = 10 -5  = 5
+		#100  Eu = 1 ; Sel = 4'b0001 ; accumulator = 2 ; tmp = 5 ;      // result = 2 -5  = -3       sign flag = 1 
+		#100  Eu = 1 ; Sel = 4'b0010 ; accumulator = 2 ; tmp = 5 ;        // result = 2 & 5 = 0      zero flag = 1 
+		#100  Eu = 1 ; Sel = 4'b0011 ; accumulator = 2 ; tmp = 5 ;        //  2 | 5 = 7
+		#100  Eu = 1 ; Sel = 4'b1111 ; accumulator = 10 ; tmp = 5 ;        // rar 00001010 -> 00000101
+		
+		#100  Eu = 1 ; Sel = 4'b0100 ; accumulator = 2 ; tmp = 5 ;        //  result = tmp - 1 
+		#100  Eu = 1 ; Sel = 4'b0110  ; accumulator = 15 ; tmp = 5 ;        //  15 ^ 5 = 7
+		
 		
 	end
 
